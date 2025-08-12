53384bf2f36265a2952c11c92b0ba8bea8540d85
arvinsoheili <soheiliarvin@gmail.com>
2025-08-12 21:54:25 +0330

    fix: fix responsive problems

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -30,7 +30,7 @@ export default function ProductCards() {
 				<CardHeader className='flex justify-center items-center'>
 					<Avatar className='w-full'>
 						<AvatarImage
-							className='rounded-xl w-full h-[25em] object-contain p-5'
+							className='rounded-xl w-full h-[15rem] xl:h-[20rem] object-contain p-5'
 							src={`images/products/${profile.image}`}
 						/>
 						<AvatarFallback className='bg-stone-600 p-5 rounded-full'>
@@ -40,11 +40,11 @@ export default function ProductCards() {
 				</CardHeader>
 				<CardContent className='gap-3 justify-center text-center h-full'>
 					<div className='gap-0.5'>
-						<h2 className='text-3xl max-xs:text-2xl font-medium break-words'>{`${profile.name}`}</h2>
-						<h4 className='text-stone-500'>{`${profile.volume}${profile.volUnit}`}</h4>
+						<h2 className='text-3xl max-xs:text-2xl xl:text-4xl font-medium break-words'>{`${profile.name}`}</h2>
+						<h4 className='text-stone-500 xl:text-xl'>{`${profile.volume}${profile.volUnit}`}</h4>
 					</div>
 				</CardContent>
-				<div className='flex justify-start text-start my-3 items-start h-full px-5'>
+				<div className='flex justify-start text-start my-3 items-start h-full px-5 xl:text-xl'>
 					<ul>
 						{profile.contains.map((skill) => {
 							return <li className='mx-0.5'>+ {skill}</li>;
@@ -52,7 +52,7 @@ export default function ProductCards() {
 					</ul>
 				</div>
 				<CardFooter className='flex flex-row gap-5 text-end justify-center items-end'>
-					<MotionButton whileHover={{ boxShadow: "0 0 10px 5px #a3e635", backgroundColor: "#a3e635" }} className='bg-lime-500 w-full'>Buy Now</MotionButton>
+					<MotionButton whileHover={{ boxShadow: "0 0 10px 5px #a3e635", backgroundColor: "#a3e635" }} className='bg-lime-500 w-full xl:text-xl py-6'>Buy Now</MotionButton>
 				</CardFooter>
 			</MotionCard>
 		);
@@ -64,7 +64,7 @@ export default function ProductCards() {
 				variants={gridContainerVariants}
 				initial='hidden'
 				animate='show'
-				className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-15 mx-15'
+				className='grid grid-cols-1 min-[580px]:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-15'
 			>
 				{profileCards}
 			</motion.section>
