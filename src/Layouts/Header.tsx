16b33c4998af5062ff4fb8f9e73a74c6837da5d9
arvinsoheili <soheiliarvin@gmail.com>
2025-08-19 14:24:17 +0330

    feat: add navbar to header

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,9 +1,12 @@
+import Navigation from "./UI/Navigation";
 
 export default function Header() {
+    const menu = ['welcome', 'products'];
     
     return (
-        <header className="min-w-screen ">
-
+        <header className="min-w-screen bg-neutral-800 py-8">
+            <Navigation items={menu} />
+            
         </header>
     );
 }
